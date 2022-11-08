2f3889eb1b4f9c35525a790da35d6eb113516a34
ZhouSusan <szhou089@gmail.com>
2022-11-08 02:37:31 -0800

    added displayUserMenu() in AddressBookApp.java

--- src/main/java/org/example/AddressBookApp.java
+++ src/main/java/org/example/AddressBookApp.java
@@ -3,9 +3,14 @@ package org.example;
 import java.util.Scanner;
 
 public class AddressBookApp {
+    public static void displayUserMenu() {
+        System.out.println("Welcome to your personal address book. Please enter : \n1 - Add a new Contact \n2 - Print out all contacts" +
+                "\n3 - Delete a contact \n4 - Total number of contacts so far in your address book \n5 Quit");
+    }
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
         AddressBook myAddressBook = new AddressBook();
+        displayUserMenu();
     }
 }
