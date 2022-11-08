f1396c1a39ad0813ea136ab022cbc43ee912e18a
ZhouSusan <szhou089@gmail.com>
2022-11-08 01:10:02 -0800

    added Class Entry and Entry.java file

---
+++ src/main/java/org/example/Entry.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Entry {
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String phoneNumber;
+    private String email;
+
+    public Entry(String firstName, String lastName, String address, String phoneNumber, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+}
