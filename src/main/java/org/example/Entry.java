da6176148ba8d11e71caa8f1b87242bb2b4193a2
ZhouSusan <szhou089@gmail.com>
2022-11-08 01:36:02 -0800

    added getters and setters in Entry.java

--- src/main/java/org/example/Entry.java
+++ src/main/java/org/example/Entry.java
@@ -7,6 +7,46 @@ public class Entry {
     private String phoneNumber;
     private String email;
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
     public Entry(String firstName, String lastName, String address, String phoneNumber, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
