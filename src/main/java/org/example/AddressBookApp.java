f948789c99f2c1748c2c769788e87bf277f4bea5
ZhouSusan <szhou089@gmail.com>
2022-11-08 02:55:37 -0800

    added while loop and swith statements in AddressBookApp.java

--- src/main/java/org/example/AddressBookApp.java
+++ src/main/java/org/example/AddressBookApp.java
@@ -12,5 +12,48 @@ public class AddressBookApp {
         Scanner input = new Scanner(System.in);
         AddressBook myAddressBook = new AddressBook();
         displayUserMenu();
+        int userChoice = input.nextInt();
+
+        while (userChoice != 5) {
+            switch (userChoice) {
+                case 1:
+                    if (myAddressBook.addressBookSize() < 101) {
+                        System.out.println("New Entry: \nEnter new person's name: ");
+                        String name = input.next();
+                        System.out.println("Enter Address: ");
+                        String address = input.next();
+                        System.out.println("Phone Number:");
+                        String phoneNumber = input.next();
+                        System.out.print("Email: ");
+                        String email = input.next();
+
+                        myAddressBook.addEntry(new Entry(name,address, phoneNumber, email));
+                        break;
+                    } else {
+                        System.out.println("Sorry, this address book is full!");
+                        break;
+                    }
+
+                case 2:
+                    System.out.println("Here are all your contacts so far: ");
+                    myAddressBook.printAllContacts();
+                    break;
+
+                case 3:
+                    System.out.println("Please enter the name of the contact, you wish to delete? ");
+                    String deleteName = input.next();
+                    myAddressBook.deleteEntry(deleteName);
+                    break;
+
+                case 4:
+                    System.out.format("You have a total of %s contacts in your address book.", myAddressBook.addressBookSize());
+                    break;
+                default:
+                    System.out.println("Invalid choice! Please select 1 - 5.");
+                    break;
+            }
+            displayUserMenu();
+            userChoice = input.nextInt();
+        }
     }
 }
