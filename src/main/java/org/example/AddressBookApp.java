5304bfd8935b76884e5fb24f90a3779eb9f8b042
ZhouSusan <szhou089@gmail.com>
2022-11-08 02:19:30 -0800

    imported Scanner and created new object for scanner(input) in AddressBookApp.java

--- src/main/java/org/example/AddressBookApp.java
+++ src/main/java/org/example/AddressBookApp.java
@@ -1,6 +1,10 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class AddressBookApp {
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
     }
 }
