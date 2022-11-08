f1023e9e19cab3164a382a47bb3c97915658b6c3
ZhouSusan <szhou089@gmail.com>
2022-11-08 01:52:56 -0800

    created AddressBook class with ArrayList and addEntry() in AddressBook.java

---
+++ src/main/java/org/example/AddressBook.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class AddressBook {
+    private ArrayList<Entry> entries = new ArrayList<Entry>();
+
+    public void addEntry(Entry e) {
+        entries.add(e);
+    }
+
+    
+}
