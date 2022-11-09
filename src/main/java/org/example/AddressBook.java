8fa88320c2a21fc4cc0a55dcf33ef1bda6416c47
ZhouSusan <szhou089@gmail.com>
2022-11-08 16:10:44 -0800

    added SearchEntry() feature in AddressBook.java and updated while loop and switch statment in AddressBookApp.java

--- src/main/java/org/example/AddressBook.java
+++ src/main/java/org/example/AddressBook.java
@@ -26,4 +26,13 @@ public class AddressBook {
             System.out.println("\n*******\n" + e.getEntryInfo());
         }
     }
+
+    public String searchEntry(String thisEntry) {
+        for (int i = 0 ; i < entries.size(); i++) {
+            if (entries.get(i).getName().equals(thisEntry)) {
+                System.out.format("\nHere is the entry that you have selected: %s", entries.get(i).getEntryInfo());
+            }
+        }
+        return "Sorry, this an invalid entry, and it does not exist in your address nook. Please try again!";
+    }
 }
--- src/main/java/org/example/AddressBookApp.java
+++ src/main/java/org/example/AddressBookApp.java
@@ -4,8 +4,12 @@ import java.util.Scanner;
 
 public class AddressBookApp {
     public static void displayUserMenu() {
-        System.out.println("\nWelcome to your personal address book. Please enter a number 1 - 5.  \n1 - Add a new Contact \n2 - Print out all contacts" +
-                "\n3 - Delete a contact \n4 - Total number of contacts so far in your address book \n5 Quit \nPlease Enter choice here : ");
+        System.out.println("\nWelcome to your personal address book. Please enter a number 1 - 5.  \n1 - Add a new Contact " +
+                "\n2 - Print out all contacts" +
+                "\n3 - Delete a contact " +
+                "\n4 - Total number of contacts so far in your address book " +
+                "\n5 - Search Directory for a specific contact" +
+                "\n6 Quit \nPlease Enter choice here : ");
     }
     public static void main(String[] args) {
 
@@ -14,11 +18,11 @@ public class AddressBookApp {
         displayUserMenu();
         int userChoice = input.nextInt();
 
-        while (userChoice != 5) {
+        while (userChoice != 6) {
             switch (userChoice) {
                 case 1:
                     if (myAddressBook.addressBookSize() < 101) {
-                        System.out.println("New Entry: \nEnter new person's name (lastName, firstname (no spaces)) : ");
+                        System.out.println("New Entry: \nEnter new person's name (lastName,firstname (no spaces)) : ");
                         String name = input.next();
                         System.out.println("Phone Number (***-***-****) no spaces:");
                         String phoneNumber = input.next();
@@ -49,6 +53,14 @@ public class AddressBookApp {
                 case 4:
                     System.out.format("You have a total of %s contacts in your address book.", myAddressBook.addressBookSize());
                     break;
+
+                case 5:
+                    System.out.println("Please enter the name of the contact, you wish to search in your directory:" +
+                            "Please enter this desired name in this format (last name,first name) with no spaces.");
+                    String searchName = input.next();
+                    myAddressBook.searchEntry(searchName);
+                    break;
+
                 default:
                     System.out.println("Invalid choice! Please select 1 - 5.");
                     break;
